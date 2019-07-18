ba807e10907a964915aefb2302c051014a2ed40d
Kalloor <ericabraham.ea@gmail.com>
2019-07-18 19:18:21 +0530

    Delete File from upload dir

--- worker/delurl.php
+++ worker/delurl.php
@@ -4,10 +4,27 @@ if (!($_SESSION['login'])) {
     header('Location: login.php');
 }
 
+
+
 include("../config/config.php");
 if (isset($_REQUEST['deleteurlid'])) {
     $delid = mysqli_real_escape_string($conn, $_POST['deleteurlid']);
     $login_id = $_SESSION['login_id'];
+
+
+    //Try to delete file from server
+    $sql = "SELECT * FROM linklist WHERE linkid='$delid' AND clientid='$login_id'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if (strpos($row['link'], 'autotab_3c/upload/') !== false) {
+                echo $path= str_replace("http://". gethostname() ."/autotab_3c/upload/","upload/",$row['link']);
+                chdir("..");
+                unlink($path);
+              
+            }
+        }
+    }    
 
     echo $sql = "DELETE FROM linklist WHERE linkid='$delid' AND clientid='$login_id'";
     //echo $sql;
