30e8b1d451c00c43256e4ef702c2c502a0e8a4c1
Kalloor <ericabraham.ea@gmail.com>
2019-07-18 20:08:25 +0530

    Error Page Added

--- api/request.php
+++ api/request.php
@@ -3,24 +3,36 @@ include("../config/config.php");
 
 
 //Get day of week
-$dayofweek=date('w');
+$dayofweek = date('w');
 $days = array("sun", "mon", "tue", "wed", "thu", "fri", "sat");
 $todayday = $days[$dayofweek];
 
 if (isset($_REQUEST['clientid'])) {
     $clientid = mysqli_real_escape_string($conn, $_GET['clientid']);
 
- $sql = "SELECT link, duration FROM linklist WHERE clientid= '$clientid' AND status=1 AND $todayday=1";
- 
- $result = $conn->query($sql);
-    $bcount = 0;
-    $notified = 0;
+    $sql = "SELECT link, duration FROM linklist WHERE clientid= '$clientid' AND status=1 AND $todayday=1";
+
+    $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
 
             echo $row['link'] . " " . $row['duration'] . "\n";
         }
-    } else
-        echo "No links to display";
-} else
-    echo "Invalid client ID";
+    } else {
+        $sql = "SELECT link FROM linklist WHERE clientid= '$clientid'";
+
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            echo "http://". gethostname() ."/autoTab_3c/player/errornoactivetab.html 30";
+        } else {
+            $sql = "SELECT client FROM client WHERE clientid= '$clientid'";
+
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                echo "http://". gethostname() ."/autoTab_3c/player/errornotab.html 30";
+            }else {
+                echo "http://". gethostname() ."/autoTab_3c/player/errorinvalidclient.html 30";
+            }
+        }
+    }
+}
