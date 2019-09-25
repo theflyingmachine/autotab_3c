39ffafcf0f9592609d2260b8bf32b7e3d7ea1c6c
Kalloor <ericabraham.ea@gmail.com>
2019-09-25 18:09:23 +0530

    get licence key API added

---
+++ api/getkey.php
@@ -0,0 +1,24 @@
+<?php
+include("../config/config.php");
+
+
+if (isset($_REQUEST['username'])) {
+    $username = mysqli_real_escape_string($conn, $_GET['username']);
+    if (isset($_REQUEST['password'])) {
+        $password = mysqli_real_escape_string($conn, $_GET['password']);
+        $password=md5($password);
+    } else {
+       echo "Username and password needed";
+       exit;
+    }
+   
+   
+    $sql = "SELECT licencekey FROM client WHERE client= '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo $licencekey =  $row['licencekey'];
+    }else{
+        echo "Invalid Username or Password";
+    }
+}
