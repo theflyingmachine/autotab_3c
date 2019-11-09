c9279c93edf15fffa643d33c3325a1c7f668dd63
Eric Abraham <ericabraham.ea@gmail.com>
2019-11-09 14:29:09 +0530

    Driver support added

---
+++ driver/updates/autoStartupdate.bat
@@ -0,0 +1,20 @@
+@echo off
+tasklist /fi "IMAGENAME eq autoTab launcher.exe" 2>NUL | find /I /N "autoTab launcher.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+rem msg * Program is running
+goto Exit
+)
+if "%ERRORLEVEL%"=="1" (
+rem msg * Program is not running
+rem kill all/any chrome process
+rem echo "killing Chrome"
+rem taskkill /F /IM chrome.exe
+echo "Killing Chrome driver"
+taskkill /F /IM chromedriver.exe
+rem restart autotab
+wmic datafile where name="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" get Version /value > C:\autoTab\Driver\browserversion
+Powershell.exe -executionpolicy remotesigned -File  C:\autoTab\Driver\updatedriver.ps1
+start "AutoTab" "C:\autoTab\AutoTab.exe" updatepoc
+goto Exit
+)
+pause
